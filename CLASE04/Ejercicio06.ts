e7cf87a1ad3c18605f4916bcc66093195fe9159c
CinthiaRod <Cinthiarb28@gmail.com>
2025-04-09 17:25:05 -0700

    Ejercicio 6

---
+++ CLASE04/Ejercicio06.ts
@@ -0,0 +1,31 @@
+// Actividad 6: Filtrar elementos de un arreglo genérico usando For...Of y Aserciones
+// Crea una función genérica llamada filtrarElementos que reciba un arreglo de valores mixtos (por ejemplo, number | string | boolean). 
+// Utiliza aserciones de tipo para acceder a las propiedades específicas de string.
+
+function filtrarElementos<T>(arreglo: T[]): string[] {
+    const cadenas: string[] = [];
+
+// Usando un bucle for...of, filtra solo los valores que sean cadenas de texto (string). 
+    for (const elemento of arreglo) {
+      if (typeof elemento === 'string') {  //Condición para que sea extrictamente a string
+        const cadena = elemento as string;
+        cadenas.push(cadena);
+        console.log(`Cadena encontrada: ${cadena}, Longitud: ${cadena.length}`);
+      };
+    };
+    return cadenas;
+  };
+
+  //Declaramos una arreglo con valores mixtos
+  const valoresMixtos: (number | string | boolean)[] = [
+    12,
+    "Hola mundo",
+    true,
+    "Me encanta Back",
+    false,
+    "TypeScript",
+    3
+  ];
+//   //Aplicamos el filtro sobre el arreglo de valores mixtos
+const resultadoFiltro = filtrarElementos(valoresMixtos);
+  
