61d68c68e5f54180ad8673a23bec424259302973
CinthiaRod <Cinthiarb28@gmail.com>
2025-04-25 21:29:35 -0700

    Ejercicios clase 5

---
+++ CLASE05/Ejercicio05.ts
@@ -0,0 +1,26 @@
+// Ejercicio 5: Interfaz
+// 1.Define una interfaz llamada IPersona con propiedades nombre, edad y un método presentarse().
+interface IPersona{
+    nombre: string,
+    edad: number,
+    presentarse: () => void;
+ };
+
+// 2.Crea una clase llamada Persona que implemente esta interfaz y proporciona la implementación del método presentarse().
+class Persona implements IPersona {
+    nombre: string;
+    edad: number;
+  
+    constructor(nombre: string, edad: number) {
+      this.nombre = nombre;
+      this.edad = edad;
+    };
+  
+    presentarse(): void {
+      console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    };
+  };
+
+  //Ejemplo
+  const persona = new Persona("Cinthia", 28);
+persona.presentarse();
