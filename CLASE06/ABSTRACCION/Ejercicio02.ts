4bc03ce39b861f3de0a5e125537c1e063289bbf7
CinthiaRod <Cinthiarb28@gmail.com>
2025-05-01 18:43:05 -0700

    Código del ejercicio 2

--- CLASE06/ABSTRACCION/Ejercicio02.ts
+++ CLASE06/ABSTRACCION/Ejercicio02.ts
@@ -1,24 +1,40 @@
 // Ejercicio 2: Clases Abstractas - Sistema de Vehículos
 // Crea una clase abstracta Vehiculo con una propiedad velocidad y un método abstracto mover(). 
-interface Vehiculo { 
-    mover(): void; // Método abstracto en la interfaz (implícito)
-  };
+abstract class Vehiculo {
+    protected velocidad: number;
+
+    constructor(velocidad: number) {
+        this.velocidad = velocidad;
+    };
+
+    abstract mover(): void;
+};
   
 // Luego, implementa dos clases concretas: Auto y Bicicleta. 
-class Auto implements Vehiculo {
-    mover(): void {
-      console.log("Moviendo el auto...");
+class Auto extends Vehiculo {
+    constructor(velocidad: number) {
+        super(velocidad);
     };
-  };
 
-  class Bicicleta implements Vehiculo {
     mover(): void {
-      console.log("Moviendo la bicicleta...");
+        console.log(`Moviendo el auto a ${this.velocidad} km/h...`);
     };
-  };
+};
+
+
+class Bicicleta extends Vehiculo {
+    constructor(velocidad: number) {
+        super(velocidad);
+    };
+
+    mover(): void {
+        console.log(`Moviendo la bicicleta a ${this.velocidad} km/h...`);
+    };
+};
+
 // Usa los modificadores de acceso para controlar la visibilidad de las propiedades.
-const auto = new Auto();
-const bicicleta = new Bicicleta();
+const auto = new Auto(50);
+const bicicleta = new Bicicleta(8);
 
 auto.mover();
 bicicleta.mover();
